d98d000b3962a8c151793b2ac0b02adf4ae74ab3
Imdat Solak <imdat@solak.de>
2023-03-23 07:53:42 +0100

    Copied AVFoundationVideoPlayer as Interface to macOS

--- packages/video_player/video_player_macos/lib/video_player_macos.dart
+++ packages/video_player/video_player_macos/lib/video_player_macos.dart
@@ -1,3 +1,1 @@
-// The video_player_platform_interface defaults to MethodChannelVideoPlayer
-// as its instance, which is all the macOS implementation needs. This file
-// is here to silence warnings when publishing to pub.
+export './src/video_player_macos.dart';
